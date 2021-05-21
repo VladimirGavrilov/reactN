3be06df25b79f137963aca073b798599ef9d499b
VladimirGavrilov <gavriloff.v@yandex.ru>
2021-05-21 14:53:49 +0300

    210521 add form2

--- src/content/itogToVote/itogToVote.js
+++ src/content/itogToVote/itogToVote.js
@@ -11,24 +11,32 @@ function ItogToVote(props) {
     const listGrups = cont.filter((gr) => gr.length == 4);
     let buttonToVote = React.createRef();
     let toVote = () => {
-        let toV = buttonToVote.value
+        let toV = buttonToVote.current.value
         return alert(`${toV}`);
     }
-  
+    // debugger;
     const listGrup = listGrups.map(lGR => lGR.map((lgr) => (
         
-     
+        
         <div id={lgr.name_en} className={st.itogContry}  >
             <h6>{lgr.name_ru} > {lgr.value} голосов</h6>
             <div class="progress">
-                <progress max="100000000000" value={lgr.value}></progress>
+                <progress max="10000000" value={lgr.value}></progress>
                 <div class="progress-value"></div>
                 <div class="progress-bg"><div class="progress-bar"></div></div>
             </div>
 
             <form>
-                <NumericInput ref={buttonToVote} min={0} max={10000000} value={this.value || 10} />
-                
+                <input ref={buttonToVote} type="number" step="10" min="0" max="70" value="10" />
+                <NumericInput
+                    className="form-control"
+                    value={50}
+                    min={0}
+                    max={100}
+                    step={1}
+                    precision={0}
+                    size={5}
+                />
             <input type="submit" onClick= {toVote}/>
             </form>
             
